881840aa3b98aa1e92b4dbd91c12bee7672f106a
TheOneYana <yanatheone@yahoo.com>
2023-04-09 16:26:09 -0700

    kitten images added, react-router set

---
+++ src/components/Logo.js
@@ -0,0 +1,9 @@
+import logo from "../images/NoseShadow.png"
+
+const Logo = () => {
+  return (
+    <img src={logo} className='logo'/>
+  )
+}
+
+export default Logo;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import About from "./About";
+import Error from "./Error";
+import Landing from "./Landing";
+import Contact from "./Contact"
+import Gallery from './Gallery';
+import Register from './Register';
+import OurClients from './OurClients';
+
+export {Landing, Error, Register, About, Gallery, OurClients, Contact};
