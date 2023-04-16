2f3b20a57e57d006a68ed075a37dddeec7f401cc
TheOneYana <yanatheone@yahoo.com>
2023-04-15 18:46:54 -0700

    mobile responsivness adjusted, CRUD functionality fixed, styling added to buttons of CRUD page

--- src/components/Crud.js
+++ src/components/Crud.js
@@ -1,28 +1,36 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import paw2 from '../images/paw2.png'
-
+import { useDispatch, useSelector  } from 'react-redux';
+import paw2 from '../images/paw2.png';
+import CrudItem from './CrudItem';
+import {fetchCats} from '../features/crud/crudSlice';
+import CrudWrap from '../wrappers/CrudWrap';
 const Crud = () => {
-  const {cats,total} = useSelector((state)=>state.cats)
+const dispatch = useDispatch();
+const {catItems,total} = useSelector((store)=>store.cats)
+
 const deleteItm = () => {
   console.log(`deleted`)
 }
   if(total< 1){
     return (
+      <CrudWrap>
       <section className='cats'>
-        <p>No cats left!</p>
-        <img src={paw2}/>
+        <p>Let's call the cats!</p>
+       <button className='crudBtn' onClick={()=> dispatch(fetchCats())}>Psspsspss</button>
+       <img src={paw2}/>
       </section>
+      </CrudWrap>
     )
   }
   return (
-    <div className='kittensCrud'>{cats.map((cat)=>(
-      <div className='grid-item' key={cat.id}>
-      <p>{cat.name}</p>
-      <img className='kittenImg'src={cat.img}/>
-      <p>{cat.avail}</p>
-      <button onClick={()=>deleteItm(cat.id)}>delete</button>
-      </div>))}
-      </div>)}
+    <CrudWrap>
+    <section>
+      <div className='kittensCrud'>
+        {catItems.map((cat)=> {
+          return <CrudItem key={cat.id} {...cat}/>
+        })}
+      </div>
+    </section>
+    </CrudWrap>
+   )}
 
 export default Crud;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App";
-import {Provider} from 'react-redux';
 import {store} from './store';
+import {Provider} from 'react-redux';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
