2c02f8103231648a3f0b7fe5d7bebdfa12c2a872
TheOneYana <yanatheone@yahoo.com>
2023-04-16 15:46:23 -0700

    galleryPaginationSlice

---
+++ src/features/pagination/galleryPaginationSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import cats from '../../cats';
+const initialState = {
+    imgs:cats,
+    imgsPerPage: 8,
+    currentPage: 1
+    
+}
+const galleryPagination = createSlice({
+    name: 'imgs',
+    initialState,
+    reducers:{
+        onNavigateNext: (state)=>{
+            state.currentPage++;
+        },
+        onNavigatePrev: (state)=>{
+            state.currentPage--;
+        },
+        onChangeImgsPage: (state, action)=>{
+        state.imgsPerPage = action.payload;
+        },
+        onCLickCurrentPage: (state, action)=>{
+        state.currentPage = action.payload;
+        } 
+    }
+});
+
+export const {onNavigateNext, onNavigatePrev,onChangeImgsPage,onCLickCurrentPage} = galleryPagination.actions;
+
+export default galleryPagination.reducer;
+
