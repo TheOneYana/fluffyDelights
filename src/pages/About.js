c1ea3f2145fad18d83dea01916246b6884823f9d
TheOneYana <yanatheone@yahoo.com>
2023-04-21 00:12:40 -0700

    About page content added

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <p>We appreciate your interest in our cattery! We are Yana and Alena, the founders of Fluffy Delights which was established in 2019. Our passion for cats and their well-being has inspired us to create a place where we can breed healthy and beautiful British Shorthair kittens. 
+
+    Our goal is to provide our clients with the best possible experience when adopting a kitten from our cattery. We take great pride in the care and attention we give to each kitten we raise, ensuring that they are socialized, healthy, and ready to become a cherished member of your family.
+    
+    We understand that purchasing a new kitten can be a big decision, which is why we take great care to ensure that our cats are of the highest quality. Our British Shorthair cats are known for their sweet personalities, plush coats, and striking looks. We also take great care to ensure that our cats are well socialized and ready to adapt to their new homes.
+    
+    Our cattery have produced over 30 kittens so far, and we couldn't be happier with the feedback we've received from our clients. We are grateful to have such wonderful people who share our love and passion for cats. We believe that every kitten we raise is special and unique, and we take great pride in helping our clients find their perfect match. Thank you for considering our cattery for your next furry family member. We look forward to hearing from you and helping you find your perfect kitten!</p>
+    <h3>Adoption process:</h3>
+    <p>As a small cattery, we only have two litters per year. To check if any kittens are currently available for adoption, please visit the "Available Kittens" section of our website. In the event that no kittens are currently available, please do not hesitate to contact us for information on upcoming litters.</p>
+    <p>If you find an available kitten that you would like to adopt, please contact us to schedule a visit. Kittens are typically ready to go to their new homes when they reach twelve weeks of age. At eight weeks old, we take them to their first veterinary appointment for a check-up and to receive their necessary shots.</p>
+    <h3>Signing a contract:</h3>
+    <p>After adopting your kitten, we kindly request that you schedule an appointment with your veterinarian to spay or neuter them when the kitten turns five months old. Spaying or neutering is crucial to avoid many health issues, including cancer, and we ask all of our clients to sign a contract stating that they will spay or neuter their kitten when it turns five months old.</p>
+    <p>We are committed to ensuring that each of our kittens finds a loving and caring home. Our adoption process is designed to help potential adopters find the perfect match for their family, while also ensuring the well-being of our kittens. If you have any questions about the adoption process or any of our kittens, please do not hesitate to contact us. We look forward to hearing from you and helping you find your new furry friend.</p>
+    </div>
   )
 }
 
-export default About
+export default About;
