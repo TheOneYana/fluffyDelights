499f2fe53153ae1ec201b85bb5a0cebd6eb88e8c
TheOneYana <yanatheone@yahoo.com>
2023-04-21 00:13:14 -0700

    Logo now routes to Landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 return (
 <Wrapper>
 <div className="top-logo-container">
-<img src={logo} className='logo'/>
+<Link to='/'><img src={logo} className='logo'/></Link>
 </div>
 <nav className="nav"><Link to='/' className="h4">Home</Link><Link to='/About' className="h4">About</Link><Link to='/Gallery' className="h4">Gallery</Link><Link to='/OurClients' className="h4">Our CLients</Link><Link to='/Contact' className="h4">Contact</Link><Link to="https://flip-card-memory-game.netlify.app"  className="h4">Cat Game!</Link></nav>
 </Wrapper>
