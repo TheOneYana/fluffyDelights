6f86adfe62ea25ce24a4a71b01d462fdc0ac176f
TheOneYana <yanatheone@yahoo.com>
2023-04-29 17:02:28 -0700

    added available kittens data, added our clients data, removed unnecessary files

--- src/pages/Available.jsx
+++ src/pages/Available.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import kitten from '../utils/availKittensData';
+import CrudWrap from '../wrappers/CrudWrap';
+import { useNavigate, useParams } from "react-router-dom";
 
 const Available = () => {
-  return (
-    <div>Available</div>
+  const navigate = useNavigate();
+  const ask =()=>{
+    navigate({pathname: '/contact'})
+}
+ return (
+  <CrudWrap>
+    <div className='availKittens'>
+    {kitten.map((cat)=>  <div className='grid-item' key={cat.id}>
+  <p>{cat.color} {cat.sex}</p>
+  <img className='kittenImg'src={cat.img} alt='cat'/>
+  {/* <p>{avail}</p> */}
+  <button className='crudBtn' onClick={()=> ask()}>Ask question</button>
+  </div>
   )
 }
+</div>
+  </CrudWrap>
+    )
+}
+
 
 export default Available;
