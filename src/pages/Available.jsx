1505665ca04fe271bcb07b08e66dad2494072ad9
TheOneYana <yanatheone@yahoo.com>
2023-06-11 10:12:53 -0700

    Available kittens updated, babel updated

--- src/pages/Available.jsx
+++ src/pages/Available.jsx
@@ -2,8 +2,16 @@ import kitten from '../utils/availKittensData';
 import CrudWrap from '../wrappers/CrudWrap';
 import { useNavigate, useParams } from "react-router-dom";
 import {Helmet} from 'react-helmet-async';
+import { useState, useEffect } from 'react';
+import Loading from '../components/Loading';
 
 const Available = () => {
+const [loading, setLoading] = useState(true);
+useEffect(()=>{
+setTimeout(()=>{
+  setLoading(false);
+}, 2000)
+}, []);
   const navigate = useNavigate();
   const ask =()=>{
     navigate({pathname: '/contact'})
@@ -11,10 +19,12 @@ const Available = () => {
  return (
   <CrudWrap>
     <Helmet>
+  
     <title>Available British Shorthair kittens</title>
     <meta name = 'description' content = 'If we have any available british shorthair kittens, they will be displayed here. If none of the kittens are available, feel free to use CONTACT section to ask us about upcoming litters. Currently we have lilac boy and lilac girl available in ut cattery.'/>
     <link rel='canonical' href='/Available'/>
   </Helmet>
+  {loading ? (<Loading/>):(
     <div className='availKittens'>
     {kitten.map((cat)=>  <div className='grid-item kittens' key={cat.id}>
   <p>{cat.color} {cat.sex}</p>
@@ -25,6 +35,7 @@ const Available = () => {
   )
 }
 </div>
+ )}
   </CrudWrap>
     )
 }
